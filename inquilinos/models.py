afa11a0853d1298d8eec375fc1123220c5cd51c3
Roberto <robertoamaral56@gmail.com>
2022-01-18 15:52:14 -0300

    database migrations

--- inquilinos/models.py
+++ inquilinos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class inquilino(models.Model):
+    nome_inquilino = models.CharField(max_length=200)
+    idade_inquilino = models.IntegerField()
+    email_inquilino = models.EmailField()
+    telefone_inquilino = models.IntegerField()
+    sexo_inquilino = models.CharField(max_length=100)
